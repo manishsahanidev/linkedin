c849edc77813d82aa7a634dbe473b4575a57168a
Manish Sahani <work.manishsahani@gmail.com>
2024-12-30 23:24:28 +0530

    Update backend from Maven to Gradle

--- backend/src/main/java/com/linkedin/backend/BackendApplication.java
+++ backend/src/main/java/com/linkedin/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.linkedin.linkedin;
+package com.linkedin.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkedinApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LinkedinApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
